06b59a455b1a8f283aad6eb182e252064527b38e
Ad-Market <78874562+Ad-Market@users.noreply.github.com>
2022-07-26 11:47:27 +0200

    Update SwapModalFooter.tsx

--- src/components/swap/SwapModalFooter.tsx
+++ src/components/swap/SwapModalFooter.tsx
@@ -152,10 +152,6 @@ export default function SwapModalFooter({
 
         {swapErrorMessage ? <SwapCallbackError error={swapErrorMessage} /> : null}
       </AutoRow>
-
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.min.js"></script>
-      <div id="container"></div>
     </>
   )
 }
